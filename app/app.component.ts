5bdedc9dd1a5e7d7ea603e99261a4909b3ee129c
IsYet1 <Don@IsYet.com>
2016-10-09 19:07:30 -0500

    Step 1 and 2. Setup.
    Ready to send the 1st request

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,12 @@
 ﻿/// <reference path="products/product-list.component.ts" />
 
 import { Component } from 'angular2/core';
+import { HTTP_PROVIDERS } from 'angular2/http';
+import 'rxjs/Rx'; // Load all features
+
 import { ProductListComponent } from './products/product-list.component';
-import  { ProductService } from './products/product.service';
+import { ProductService } from './products/product.service';
+
 
 @Component({
     selector: 'pm-app',
@@ -12,7 +16,7 @@ import  { ProductService } from './products/product.service';
     </div>
     `,
     directives: [ProductListComponent],
-    providers: [ProductService]
+    providers: [ProductService, HTTP_PROVIDERS]
 })
 
 
